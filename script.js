b55badb313510e7c25ce27c1552773a9d26e567a
souvikkoley9674 <113795422+souvikkoley9674@users.noreply.github.com>
2023-01-19 16:39:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let qr_code = document.querySelector("#qr-code");
+let code_img = document.querySelector("#code-img");
+let loader = document.querySelector("#loading");
+
+qr_code.addEventListener("click", () => {
+    // loader.style.display = "block";
+
+    code_img.onload = function () {
+        loader.style.diaplay = "none";
+    }
+    let input = document.querySelector("#input").arrValueMax;
+    let api = `https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=${input}`;
+
+    code_img.src = api;
+   
+})
